b3da5bdedaa1055e1bec568f7eb434eff0c9a3e2
Aliaksei Antonovich <glados.lexa@gmail.com>
2023-01-26 18:08:23 +0300

    added random

--- Example/Example003_summation/Program.cs
+++ Example/Example003_summation/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 5;
-int numberB = 12;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int summa;
 summa = numberA + numberB;
 Console.Write(summa);
