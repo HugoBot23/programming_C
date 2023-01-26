8eff938ce42ff22cb5aff2ce81aa468670c01544
Aliaksei Antonovich <glados.lexa@gmail.com>
2023-01-26 19:13:36 +0300

    Example005_If_Else

---
+++ Example/Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя ");
+string username = Console.ReadLine();
+  
+if(username.ToLower() == "алексей")
+{
+  Console.Write("Ооо привет, Алексей");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.Write(username);
+}
